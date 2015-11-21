5b9e6d890b3e9ccf00a77b48efb535392bf28d18
sugarfreeva <asweet1983@gmail.com>
2015-11-21 15:25:27 -0500

    12x12 Multiplication table CodeEval challenge

--- MultiplicationTables.cpp
+++ MultiplicationTables.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "HELLO WORLD" << endl;
-	return 0;
+    // challenge wants a 12x12 matrix
+    const unsigned int MAX = 12;
+
+    for(unsigned int i = 1; i <= MAX; ++i)
+    {
+        for(unsigned int j = 1; j <= MAX; ++j)
+        {
+            printf("%4d", i*j);
+//            cout << i*j << "    ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
